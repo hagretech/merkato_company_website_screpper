5393aef0e5cace32e89966398629c234fa552a7c
kirubel demelah <kiztopia.2020@gmail.com>
2020-11-14 19:28:56 -0500

    company name added in the csv file

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ def page(url):
     for comp in data.find_all('div', class_='listing-summary-featured'):
         print('***********************************************************')
         link = comp.find('a' ,class_='pull-right')
+        company_name = comp.find('h4').text
         no += 1
         if str(link) != 'None':
             with open('company.csv', 'a', newline='', encoding='utf-8') as f:
                 writer = csv.writer(f)
-                writer.writerow([str(link.get('href'))])
+                writer.writerow([company_name,str(link.get('href'))])
+
     print(no)
 
-for i in range(400):
+for i in range(1000):
     url = 'https://www.2merkato.com/directory/' + str(i)
     page(url)
     print(url)
